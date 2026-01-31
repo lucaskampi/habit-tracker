e5732a02eb3ed33b7eddb28f015d1fa130bef245
lucaskmpz <lucasclmaia0@gmail.com>
2026-01-31 00:02:19 -0300

    feat: changing bg color to zinc-900

--- src/app/dashboard/ui/StatCard.tsx
+++ src/app/dashboard/ui/StatCard.tsx
@@ -12,10 +12,10 @@ export default function StatCard({
   caption?: string;
 }) {
   return (
-    <div className="bg-zinc-400 rounded-2xl p-5 shadow-sm border border-zinc-300 text-zinc-900">
-      <div className="text-sm text-zinc-700">{title}</div>
+    <div className="bg-zinc-900 rounded-2xl p-5 shadow-sm border border-zinc-600 text-zinc-100">
+      <div className="text-sm text-zinc-300">{title}</div>
       <div className="mt-2 text-2xl font-semibold">{value}</div>
-      {caption && <div className="mt-1 text-xs text-zinc-700">{caption}</div>}
+      {caption && <div className="mt-1 text-xs text-zinc-300">{caption}</div>}
     </div>
   );
 }
